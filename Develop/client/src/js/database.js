ddfd1931b8dfd7345f9314954c7bc01070ac96bc
ANANDNJIT <aplv180@gmail.com>
2023-12-10 15:23:14 -0500

    Ignoring specified files and directories

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
 
+  const db = await initdb();
+
+const tx = db.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+
+const result = await request;
+console.log('result.value', result);
+return result;
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (id) => {
+  console.error('getDb not implemented' , id);
+
+  const db = await initdb();
+
+const tx = db.transaction('jate', 'readwrite');
+
+const store = tx.objectStore('jate');
+
+const request = store.delete(id);
+
+const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
+
+
+
+
 
 initdb();
